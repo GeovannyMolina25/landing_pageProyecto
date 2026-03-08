3b2eff9e4ca39d3cb8c411833cc01bf10e2bb3fc
Nelson Molina <geovannysangucho25@gmail.com>
2026-03-08 01:33:13 -0500

    Estilos del Navbar

--- landing_pageProyecto/src/components/Navbar/Navbar.jsx
+++ landing_pageProyecto/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,35 @@
+import {useState} from "react"
+import styles from "./Navbar.module.css"
+
 function Navbar(){
+    const[menuOpen, setMenuOpen] = usueState(false)
+
     return(
+        <header className={styles.navbar}>
+        <div className={styles.logo}>
+            MySite
+        </div>
         <nav>
-            <h1>Logo</h1>
+            <NavLink to="/" className = {({isActive}) => isActive ? "activeLink":""}>
+                Home
+            </NavLink>
+            <NavLink to="/gallery" className = {({isActive}) => isActive ? "activeLink":""}>
+                Galeria
+            </NavLink>
+            <NavLink to="/contact" className = {({isActive}) => isActive ? "activeLink":""}>
+                Contacto
+            </NavLink>
         </nav>
+        <button
+        className={styles.humburger}
+        aria-label="Abrir menú"
+        aria-expanded={menuOpen}
+        onClick={()=> setMenuOpen(!menuOpen)}
+        >
+        ☰
+        </button>
+        </header>
     )
 }
+
+export default Navbar
